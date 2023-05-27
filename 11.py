367cc7b84fdcf96081c1a338a46bc259a74d2d5a
mrrakibkhan420 <134503086+mrrakibkhan420@users.noreply.github.com>
2023-05-27 18:00:41 +0600

    Update 11.py

--- 11.py
+++ 11.py
@@ -390,7 +390,7 @@ def follow(ses,coki):
 	get = r.find('a', string='Follow').get('href')
 	ses.get(('https://mbasic.facebook.com' + str(get)), cookies={'cookie': coki}).text
 
-logo = ("""  
+logo ="""  
 \033[1;97m                                                                                                  
 _______                                           .---.                                              
 \  ___ `'.         __.....__  .----.     .----.--.|   |          .----.     .----.                   
